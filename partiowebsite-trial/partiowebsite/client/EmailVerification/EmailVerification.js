a4cca154abf5f6f0e2bbc28b198dc70d45ce360c
Breno Alves <breno.wd@gmail.com>
2015-12-17 03:28:04 -0200

    router go

--- partiowebsite-trial/partiowebsite/client/EmailVerification/EmailVerification.js
+++ partiowebsite-trial/partiowebsite/client/EmailVerification/EmailVerification.js
@@ -10,13 +10,32 @@ Template.appLayout.onRendered(function() {
 				if (err.message == 'Verify email link expired [403]') {
 					console.log('Sorry this verification link has expired.');
 
-							bootbox.alert("Sorry this verification link has expired.");
+					bootbox.alert({
+							size: 'small',
+							message: "Sorry this verification link has expired.",
+							callback: function(){
+
+								Router.go('/');
+
+							 }
+					})
+
 				}
 			} else {
 
 				//Creating transactionsId for new user;
 				Meteor.call('createTransactions');
-					bootbox.alert("Your email address is successfully verified");
+
+				bootbox.alert({
+				    size: 'small',
+				    message: "Your email address is successfully verified.",
+				    callback: function(){
+
+							Router.go('/');
+
+						 }
+				})
+
 
 			}
 		});
