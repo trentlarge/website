b19c819f1c9565dc84cbfc8eb79546677dce59fa
Breno Alves <breno.wd@gmail.com>
2015-12-17 02:48:41 -0200

    kadira install

--- partiowebsite-trial/partiowebsite/server/server.js
+++ partiowebsite-trial/partiowebsite/server/server.js
@@ -1,9 +1,12 @@
+
+Kadira.connect('HBDrxrGMwsaZo8juo', '854de5d4-1bf7-4eef-999d-b5f10f39952b');
+
 Meteor.methods({
 	addEmail: function(email) {
 		checkEmail = Match.Where(function (x) {
 			check(x, String);
 			return RegExp(/^\S+@\S+\.\S+$/).test(x);
-		}); 
+		});
 		alreadyExists = Match.Where(function(y) {
 			return !Emails.find({email: y}).count();
 		})
@@ -14,7 +17,7 @@ Meteor.methods({
 		try {check(email, alreadyExists)} catch (err) {
             throw new Meteor.Error('Already-Exists')
         }
-        
+
 		Emails.insert({email: email});
 	},
     sendEmail: function (to, from, subject, text) {
